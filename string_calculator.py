cd236296270882d206f949cd279619bf32ea1e85
Ejonas <jonas.gift83@gmail.com>
2020-11-20 01:01:00 +0200

    initial commit

---
+++ string_calculator.py
@@ -0,0 +1,23 @@
+import re
+
+
+class Calculator:
+
+    def add(self, numbers):
+        if numbers == '':
+            return 0
+
+        digits = list(map(int, re.findall(r'-?\d+\.?\d*', numbers)))
+        digits_less_than_thousand = list(filter(lambda x: x < 1000, digits))
+        negative_numbers = list(filter(lambda x: x < 0, digits))
+        listToStr = ' '.join([str(elem) for elem in negative_numbers])
+
+        if negative_numbers:
+            raise Exception(f'negatives not allowed {listToStr}')
+
+        return sum(digits_less_than_thousand)
+
+
+if __name__ == '__main__':
+    calculate = Calculator()
+    calculate.add('5,4,100')
---
+++ test_string_calculator.py
@@ -0,0 +1,63 @@
+from string_calculator import Calculator
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    calculate = Calculator()
+
+    def test_empty_string(self):
+        """it should return 0 if the string is empty"""
+        value = self.calculate.add('')
+        self.assertEqual(0, value)
+
+    def test_add_two_CSVs(self):
+        """it should add 2 numbers in a comma separated string"""
+        value = self.calculate.add('1, 2')
+        self.assertEqual(3, value)
+
+    def test_add_multiple_CSVs(self):
+        """it should an unknown amount of numbers in a comma separated string"""
+        value = self.calculate.add('1, 2, 3')
+        self.assertNotEqual(3, value)
+        self.assertEqual(6, value)
+
+    def test_new_line_as_delimiter(self):
+        """it should be able to use new lines as delimiters"""
+        value = self.calculate.add('1\n2,3')
+        self.assertEqual(6, value)
+
+    def test_different_delimiters(self):
+        """it should be able handle different delimiters"""
+        value = self.calculate.add('//;\n1;2')
+        self.assertEqual(3, value)
+
+    def test_negative_numbers_exception(self):
+        """it should raise an exception containing all negative numbers if any are found"""
+        with self.assertRaises(Exception) as context:
+            self.calculate.add('-1, -2, -3, 1, 2, 3')
+        self.assertTrue('negatives not allowed -1, -2, -3', context.exception)
+
+    def test_bigger_than_1000(self):
+        """it should ignore numbers bigger than 1000"""
+
+        value = self.calculate.add('//;\n1000,1;2')
+        self.assertEqual(3, value)
+
+    def test_delimiters_any_length(self):
+        """it should ignore numbers bigger than 1000"""
+        value = self.calculate.add('//[***]\n1***2***3')
+        self.assertEqual(6, value)
+
+    def test_multiple_delimiters(self):
+        """it should be able handle multiple delimiters"""
+        value = self.calculate.add('//[*][%]\n1*2%3')
+        self.assertEqual(6, value)
+
+    def test_multiple_delimiters_length_longer_char(self):
+        """it should be able handle multiple delimiters with length longer than one char"""
+        value = self.calculate.add('//[**][%%]\n1**2%%3')
+        self.assertEqual(6, value)
+
+
+if __name__ == '__main__':
+    unittest.main()
